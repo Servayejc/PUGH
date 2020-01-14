647d6f72b0ccd15221d6eab3e8297c90d63c7626
Servayejc <servayejc@gmail.com>
2020-01-13 22:26:40 -0500

    delete src-Copy

--- src - Copy/FTP.h
+++
@@ -1,9 +0,0 @@
-#ifndef FTP_H_
-#define FTP_H_
-#include <ESP8266WiFi.h>
-
-
-short doFTP(char* , char* , char* , char* , char*);
-short eRcv(WiFiClient aclient, char outBuf[], int size); 
-
-#endif 
